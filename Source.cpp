332a222d05f7878e3802ba00bccb230b18a64d30
Oleh <olh.masiuk@gmail.com>
2021-11-24 10:45:19 +0200

    oleh masiuk

--- Source.cpp
+++ Source.cpp
@@ -1,114 +1,224 @@
 #include <iostream>
-#include <string>
 #include <WS2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
+#include <string>
+#include <vector>
+#include <processthreadsapi.h>
+#include <Windows.h>
+#include <time.h>
+#include <stdlib.h>
+
+#pragma comment (lib, "ws2_32.lib")
 
 using namespace std;
 
-void main()
+struct MailingInfo {
+	SOCKET sock;
+	int type;
+};
+
+struct Info {
+	sockaddr_in client;
+	int clientSize;
+	char host[NI_MAXHOST];		// Client's remote name
+	char service[NI_MAXSERV];	// Service (i.e. port) the client is connect on
+	HANDLE th;
+	DWORD th_id;
+};
+
+DWORD WINAPI SendData(LPVOID lpParameter) {
+	MailingInfo* p = (MailingInfo*)lpParameter;
+
+	string isContinue;
+
+	switch (p->type) {
+	case 1: {
+		int hour = 0;
+
+		while (true) {
+			srand(time(NULL));
+			string msg = "Prediction on hour: " + to_string(hour) + " is " + to_string(rand() % 40) + " C^";
+			send(p->sock, msg.c_str(), msg.size() + 1, 0);
+			hour++;
+			Sleep(5000);
+		}
+	}
+
+	case 2: {
+		int minutes = 0;
+
+		while (true) {
+			srand(time(NULL));
+			string msg = "Prediction on minute: " + to_string(minutes) + " is " + to_string(rand() % 30) + "." + to_string(rand() % 30) + " eth.";
+			send(p->sock, msg.c_str(), msg.size() + 1, 0);
+			minutes++;
+			Sleep(5000);
+		}
+	}
+
+	case 3: {
+		int day = 0;
+
+		while (true) {
+			srand(time(NULL));
+			string msg = "Prediction on day: " + to_string(day) + " is " + to_string(rand() % 30) + "." + to_string(rand() % 30) + " usd.";
+			send(p->sock, msg.c_str(), msg.size() + 1, 0);
+			day++;
+			Sleep(5000);
+		}
+	}
+
+	}
+	return 0;
+}
+
+int SocketClientsCounter = 0;
+
+
+int main()
 {
-    string ipAddress = "127.0.0.1";            // IP Address of the server
-    int port = 54000;                        // Listening port # on the server
+	//Init winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2); //request winsock 2.2 version
 
-    // Initialize WinSock
-    WSAData data;
-    WORD ver = MAKEWORD(2, 2);
-    int wsResult = WSAStartup(ver, &data);
-    if (wsResult != 0)
-    {
-        cerr << "Can't start Winsock..." << endl;
-        exit(3);
-    }
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		cerr << "Can't Initialize winsock! Quitting" << endl;
+		exit(3); //3 - exit code to error init winsock
+	}
 
-    // Create socket
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == INVALID_SOCKET)
-    {
-        cerr << "Can't create socket..." << endl;
-        WSACleanup();
-        exit(4);
-    }
+	// Create a socket
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Fill in a hint structure
-    sockaddr_in hint;
-    hint.sin_family = AF_INET;
-    hint.sin_port = htons(port);
-    inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+	//AF_INET is ised to communicate by ipv4
+	//TCP - SOCK_STREAM
+	//UDP - SOCK_DGRAM
 
-    // Connect to server
-    int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
-    if (connResult == SOCKET_ERROR)
-    {
-        cerr << "Can't connect to server..." << endl;
-        closesocket(sock);
-        WSACleanup();
-        exit(5);
-    }
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "Can't create a socket! Quitting" << endl;
+		exit(4); //4 - exit code to error creating socket
+	}
 
-    cout << "Connecting...\n";
+	// Bind the ip address and port to a socket
+	sockaddr_in hint;				//socket for IP protocols
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);		//port number to be accessed by process
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;  //address to link socket with(INADDR_ANY - all adresses from local host - 0.0.0.0)
 
-    while (true) {
-        char buf[4096];
-        string userInput;
-        string chosedOper;
+	bind(listening, (sockaddr*)&hint, sizeof(hint)); //binds socket to port and adress
 
-        cout << "Subsciribe to:\n\n";
-        cout << "1. Hourly weather prediction\n2. Minutely stock exchange rate\n3. Daily exchange rate\n\n--> ";
-        cin >> chosedOper;
+	//places a socket in state of listening for incoming connection 
+	listen(listening, SOMAXCONN);
 
+	fd_set master;	//set of incoming sockets clients
+	FD_ZERO(&master); //clears set
+	FD_SET(listening, &master);		//sets first FD set element as server socket
 
-        int sendResult = send(sock, chosedOper.c_str(), chosedOper.size() + 1, 0);
-        if (sendResult != SOCKET_ERROR)
-        {
-            ZeroMemory(buf, 4096);
-            int bytesReceived = recv(sock, buf, 4096, 0);
-            if (bytesReceived > 0)
-            {
-                cout << "[SERVER]> " << string(buf, 0, bytesReceived) << endl;
-            }
-        }
+	vector<Info> clientsInfo;	//info about each client socket
 
-        int k = 1;
-        string isContinue;
+	while (true) {
+		fd_set copy = master;
 
-        while (true) {
+		int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
 
-            if (k % 5 == 0) {
-                cout << "[SERVER]> " << "Do you want to continue subsciption: Y | N: ";
-                cin >> isContinue;
-                int continueResult = send(sock, isContinue.c_str(), isContinue.size() + 1, 0);
+		for (int i = 0; i < socketCount; i++) {
+			SOCKET sock = copy.fd_array[i];
 
-                if (continueResult == SOCKET_ERROR) exit(5);
+			if (sock == listening) {
+				clientsInfo.push_back({});
+				clientsInfo[SocketClientsCounter].clientSize = sizeof(clientsInfo[SocketClientsCounter].client);
 
-                ZeroMemory(buf, 4096);
+				ZeroMemory(clientsInfo[SocketClientsCounter].host, NI_MAXHOST);
+				ZeroMemory(clientsInfo[SocketClientsCounter].service, NI_MAXSERV);
 
-                int bytesReceived = recv(sock, buf, 4096, 0);
-                if (bytesReceived > 0)
-                {
-                    cout << "[SERVER]> " << string(buf, 0, bytesReceived) << endl;
-                }
+				//accepts incoming connection
+				SOCKET client = accept(listening, (sockaddr*)&clientsInfo[SocketClientsCounter].client, &clientsInfo[SocketClientsCounter].clientSize);
+				if (client == SOCKET_ERROR) {
+					cerr << "Can't accept socket client incomming request.." << endl;
+				}
 
-                if (!isContinue.compare("N")) {
-                    break;
-                }
-            }
-            else {
-                ZeroMemory(buf, 4096);
+				FD_SET(client, &master);
 
-                int bytesReceived = recv(sock, buf, 4096, 0);
-                if (bytesReceived > 0)
-                {
-                    cout << "[SERVER]> " << string(buf, 0, bytesReceived) << endl;
-                }
-            }
+				if (getnameinfo((sockaddr*)&clientsInfo[SocketClientsCounter].client, sizeof(clientsInfo[SocketClientsCounter].client), clientsInfo[SocketClientsCounter].host, NI_MAXHOST, clientsInfo[SocketClientsCounter].service, NI_MAXSERV, 0) == 0)
+				{
+					cout << clientsInfo[SocketClientsCounter].host << " connected on port " << clientsInfo[SocketClientsCounter].service << endl;
+				}
+				else
+				{
+					inet_ntop(AF_INET, &clientsInfo[SocketClientsCounter].client.sin_addr, clientsInfo[SocketClientsCounter].host, NI_MAXHOST);
+					cout << clientsInfo[SocketClientsCounter].host << " connected on port " << ntohs(clientsInfo[SocketClientsCounter].client.sin_port) << endl;
+				}
 
-            k++;
-        }
-    }
+				SocketClientsCounter++;
+			}
+			else {
+				char buf[4096];
+				ZeroMemory(buf, 4096);
 
+				int bytesIn = recv(sock, buf, 4096, 0);
 
+				if (bytesIn == SOCKET_ERROR) {
+					closesocket(sock);
+					FD_CLR(sock, &master);
+				}
+				else {
+					if (!strcmp(buf, "1") || !strcmp(buf, "2") || !strcmp(buf, "3")) {
 
-    // Gracefully close down everything
-    closesocket(sock);
-    WSACleanup();
+						for (int i = 0; i < master.fd_count; i++) {
+							SOCKET outSock = master.fd_array[i];
+
+							if (outSock != listening && outSock == sock) {
+
+								string SubsciptionAccepted = "Sucessfully subscribed to mailing service...";
+
+								int oper = atoi(buf);
+
+								cout << to_string(oper) + " option of mailing was chosed by client " << clientsInfo[i - 1].host << " on port : " << clientsInfo[i - 1].service << endl;
+
+								send(outSock, SubsciptionAccepted.c_str(), SubsciptionAccepted.size() + 1, 0);
+
+								MailingInfo* mailing = new MailingInfo;
+								mailing->sock = outSock;
+								mailing->type = oper;
+
+								clientsInfo[i - 1].th = CreateThread(NULL, 0, &SendData, (void*)mailing, 0, &clientsInfo[i - 1].th_id);
+
+								if (clientsInfo[i - 1].th == NULL) {
+									cerr << "Error creating thread..." << endl;
+									exit(5);
+								}
+
+								break;
+							}
+						}
+					}
+					else if (!strcmp(buf, "N")) {
+						for (int i = 0; i < master.fd_count; i++) {
+							SOCKET outSock = master.fd_array[i];
+
+							if (outSock != listening && outSock == sock) {
+
+								TerminateThread(clientsInfo[i - 1].th, 4);
+
+								string SubsciptionCanceled = "Your subscription was cancelled.";
+								send(outSock, SubsciptionCanceled.c_str(), SubsciptionCanceled.size() + 1, 0);
+
+								cout << "Mailing was cancelled by client " << clientsInfo[i - 1].host << " on port : " << clientsInfo[i - 1].service << endl;
+
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Cleanup winsock
+	WSACleanup();
+
+	system("pause");
+
+	return 0;
 }
